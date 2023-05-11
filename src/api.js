e613debd89ec4b4eb22cbeb988666862cc2f43aa
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2023-05-11 20:08:45 +0400

    Before node version error

---
+++ src/api.js
@@ -0,0 +1,25 @@
+// tutorial app
+// https://awstip.com/express-server-on-netlify-for-free-step-by-step-guide-e5fbdb47d891
+
+const express = require("express");
+const serverless = require("serverless-http");
+
+// Create an instance of the Express app
+const app = express();
+
+// Create a router to handle routes
+const router = express.Router();
+
+// Define a route that responds with a JSON object when a GET request is made to the root path
+router.get("/", (req, res) => {
+  res.json({
+    hello: "😁😁😁"
+  });
+});
+
+// Use the router to handle requests to the `/.netlify/functions/api` path
+app.use(`/.netlify/functions/api`, router);
+
+// Export the app and the serverless function
+module.exports = app;
+module.exports.handler = serverless(app);
